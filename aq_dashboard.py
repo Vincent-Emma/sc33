6b055424b12f20e67570698ca3b76dd08ed7d67e
Vincent Emma <52681940+Vincent-Emma@users.noreply.github.com>
2020-06-26 00:06:18 -0400

    Add files via upload

--- aq_dashboard.py
+++ aq_dashboard.py
@@ -1,41 +1,51 @@
 """OpenAQ Air Quality Dashboard with Flask."""
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import requests
-import json
-from datetime import datetime
+# imports
 import openaq
-import sqlite3
+# from app import app
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import decouple
+# from decouple import config
+import requests
 
-APP = Flask(__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-APP.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-DB = SQLAlchemy(APP)
+DB = SQLAlchemy(app)
+api = openaq.OpenAQ()
 
-def retrieve_data():
-    """gets data from OpenAQ api and returns as a string of tuples"""
-    api = openaq.OpenAQ()
-    test = api.measurements(city='Los Angeles', parameter='pm25')
-    number, dict = [i for i in test]
-    results_dict = dict['results']
-    date_dict = [i['date'] for i in results_dict]
-    utc_list = [i['utc'] for i in date_dict]
-    value_list = [i['value'] for i in results_dict]
-    tuples_list = list(zip(utc_list, value_list))
-    return str(tuples_list)
 
-def retrieve_data2():
-    """gets data from OpenAQ api and returns as a list of tuples
-       used to insert into the database"""
-    api = openaq.OpenAQ()
-    test = api.measurements(city='Los Angeles', parameter='pm25')
-    number, dict = [i for i in test]
-    results_dict = dict['results']
-    date_dict = [i['date'] for i in results_dict]
-    utc_list = [i['utc'] for i in date_dict]
-    value_list = [i['value'] for i in results_dict]
-    tuples_list = list(zip(utc_list, value_list))
-    return tuples_list
+def utc_values(city='Los Angeles', parameter='pm25'):
+    """Takes in air quality measurements and returns list of utc and values"""
+    status, body = api.measurements(city='Los Angeles', parameter='pm25')
+    big_dict = body['results']
+    date = []
+    for i in big_dict:
+        for x, y in i.items():
+            if x == 'date':
+                date.append(y)
+    utc = []
+    for b in date:
+        for c, d in b.items():
+            if c == 'utc':
+                utc.append(d)
+
+    value = []
+    for p in big_dict:
+        for q, r in p.items():
+            if q == 'value':
+                value.append(r)
+
+    list_of_zipped_items = list(zip(utc, value))
+    return list_of_zipped_items
+
+
+@app.route('/')
+def root():
+    """Returns values greater than 10"""
+    risky = Record.query.filter(Record.value >= 10).all()
+    return str(risky)
 
 
 class Record(DB.Model):
@@ -43,51 +53,20 @@ class Record(DB.Model):
     datetime = DB.Column(DB.String(25))
     value = DB.Column(DB.Float, nullable=False)
 
-    # def __repr__(self, id=id, datetime=datetime, value=value):
-    #     # write a nice representation of Records'
-    #     self.id = id
-    #     self.datetime = str(datetime)
-    #     self.value = value
-    #     return '{}, {}, {}'.format(self.id, self.datetime, self.value)
-    #     # i tried returning in a nice format
-    #     # this didn't work
+    def __repr__(self):
+        return '< Date-time: {} - Value: {} >'.format(self.datetime,
+                                                      self.value)
 
 
-@APP.route('/refresh')
+@app.route('/refresh')
 def refresh():
     """Pull fresh data from Open AQ and replace existing data."""
     DB.drop_all()
     DB.create_all()
-    api_data = retrieve_data2()
+    la = utc_values(city='Los Angeles', parameter='pm25')
+    for v in la:
+        tupl = Record(datetime=v[0], value=v[1])
+        DB.session.add(tupl)
 
-    # enter the data into the sqlite3 database
-    conn = sqlite3.connect('db.sqlite3')
-    c = conn.cursor()
-    c.executemany('INSERT INTO Record (datetime,value) VALUES (?,?)', api_data)
-    conn.commit()
-    conn.close()
     DB.session.commit()
-
     return 'Data refreshed!'
-
-@APP.route('/')
-def root():
-    """Base view."""
-    data = retrieve_data()
-
-    # Get Records greater than or equal to 10
-    def greater_than_10():
-        conn = sqlite3.connect('db.sqlite3')
-        c = conn.cursor()
-        query = 'SELECT * FROM Record WHERE value >=10;'
-        answer = c.execute(query).fetchall()
-        return str(answer)
-
-    filtered_records = greater_than_10()
-    string = """ The data is: \n {}
-                 \n\n
-                 The records with values greater than or equal to 10 are: \n {}""".format(data, filtered_records)
-    return string
-
-if __name__ == '__main__':
-    APP.run(port = 5000, debug=True)
